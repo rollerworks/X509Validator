21f8dd282fa588cbea949aa07348fec87d7b1985
Sebastiaan Stok <s.stok@rollerscapes.net>
2023-11-27 11:40:48 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// See https://hupkit.github.io/hupkit/config.html
+
+return [
+    'branches' => [],
+    'host' => 'github.com',
+    'repository' => 'rollerworks/X509Validator',
+    'schema_version' => 2,
+];
